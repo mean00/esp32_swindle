e4ea8784ee2704eeca0a89d071c09ac8b0975d36
mean <fixounet@free.fr>
2026-03-23 07:21:40 +0100

    check idf env is set

--- build_all.sh
+++ build_all.sh
@@ -1,2 +1,6 @@
+if [[ -n "${IDF_PATH}" ]]; then
+  echo "Idf env not set! call idf.py"
+  exit 1
+fi
 rm -Rf target .embuild
 cargo build -p native_code && cargo build -p extra_code && cargo build -p app && bash flashme_s3.sh
--- build_mini.sh
+++ build_mini.sh
@@ -1,3 +1,7 @@
+if [[ -n "${IDF_PATH}" ]]; then
+  echo "Idf env not set! call idf.py"
+  exit 1
+fi
 rm -Rf target .embuild
 cargo build -p native_code --config 'env.ln_board="mini"' &&
   cargo build -p extra_code --config 'env.ln_board="mini"' &&
